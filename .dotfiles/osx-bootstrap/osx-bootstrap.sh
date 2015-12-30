dcb06370169efa5387f53ad80e0450882bbd8379
Paul Nikitochkin <paul.nikitochkin@gmail.com>
2015-12-30 23:26:16 +0200

    Added os x bootstrap script

---
+++ .dotfiles/osx-bootstrap/osx-bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Install Homebrew popular OS X package manager
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Setup Homebrew
+brew tap Homebrew/bundle
+brew bundle
+
+echo "Please install Sunrise Calendar from:"
+open "https://itunes.apple.com/us/app/sunrise-calendar/id886106985?ls=1&mt=12"
+
+# Setup ZSH
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Setup Log rotation
+ln -sf `realpath ./etc/newsyslog.d/io.pftg.dev.conf` /etc/newsyslog.d/io.pftg.dev.conf
+
+# SSD tricks
+sh osx-noatime.sh
+sh osx-ram-disk-for-tmp.sh
---
+++ .dotfiles/osx-bootstrap/osx-noatime.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cp Library/LaunchDaemons/io.pftg.noatime.plist /Library/LaunchDaemons
+sudo chown root:wheel /Library/LaunchDaemons/io.pftg.noatime.plist
