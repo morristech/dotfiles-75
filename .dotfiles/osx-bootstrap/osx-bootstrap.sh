e562c91340f65fc9bdcff606b47e544b1e84fead
Paul Nikitochkin <paul.nikitochkin@gmail.com>
2015-12-30 23:40:31 +0200

    Some updates

--- .dotfiles/osx-bootstrap/osx-bootstrap.sh
+++ .dotfiles/osx-bootstrap/osx-bootstrap.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+xcode-select --install
+sudo xcodebuild -license
+
 # Install Homebrew popular OS X package manager
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Setup Homebrew
 brew tap Homebrew/bundle
 brew bundle
+
+# Pull dotfiles
+vcsh clone git@github.com:pftg/dotfiles.git
 
 echo "Please install Sunrise Calendar from:"
 open "https://itunes.apple.com/us/app/sunrise-calendar/id886106985?ls=1&mt=12"
@@ -19,3 +25,8 @@ ln -sf `realpath ./etc/newsyslog.d/io.pftg.dev.conf` /etc/newsyslog.d/io.pftg.de
 # SSD tricks
 sh osx-noatime.sh
 sh osx-ram-disk-for-tmp.sh
+
+
+# Xcode
+
+
