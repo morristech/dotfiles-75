a85142e365e6c0267b006e4abbc03847c5ad663c
Paul Nikitochkin <paul.nikitochkin@gmail.com>
2016-01-04 23:48:03 +0200

    Added installing of compiled phantomjs

--- .dotfiles/osx-bootstrap/osx-bootstrap.sh
+++ .dotfiles/osx-bootstrap/osx-bootstrap.sh
@@ -1,20 +1,39 @@
 #!/bin/sh
 
-xcode-select --install
-sudo xcodebuild -license
+echo "Please install Xcode from:"
+open "https://itunes.apple.com/us/app/xcode/id497799835?ls=1&mt=12"
+read -p "Press enter to continue"
+
+echo "Please install Sunrise Calendar from:"
+open "https://itunes.apple.com/us/app/sunrise-calendar/id886106985?ls=1&mt=12"
+read -p "Press enter to continue"
 
 # Install Homebrew popular OS X package manager
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ln -s "$(brew --prefix)/Library/Contributions/brew_zsh_completion.zsh" /usr/local/share/zsh/site-functions
+
+/usr/local/bin/brew install vcsh mr
+
+# Pull dotfiles
+vcsh clone https://github.com/pftg/dotfiles.git
+
+cd ~/.dotfiles/osx-bootstrap/
+
+xcode-select --install
+sudo xcodebuild -license
 
 # Setup Homebrew
 brew tap Homebrew/bundle
 brew bundle
 
-# Pull dotfiles
-vcsh clone git@github.com:pftg/dotfiles.git
+# Install compiled phantomjs 1.9.8
+brew cask install https://raw.githubusercontent.com/caskroom/homebrew-cask/b234ca9329525eca21012c76a5bc2b69a9d15a8d/Casks/phantomjs.rb
+# Install compiled phantomjs 2.0.0
+brew cask install phantomjs
 
-echo "Please install Sunrise Calendar from:"
-open "https://itunes.apple.com/us/app/sunrise-calendar/id886106985?ls=1&mt=12"
+/usr/local/bin/brew install zsh
+chsh -s /usr/local/bin/zsh pftg
+
 
 # Setup ZSH
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
